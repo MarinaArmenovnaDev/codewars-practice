423e30d608cd1f963e0ff208e283f711dd94ab92
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-12-09 23:41:54 +0300

    first commit

---
+++ 8kyu/first..js
@@ -0,0 +1,12 @@
+//❓Ваша задача пройти только эти тесты:
+//
+// a == false
+// !a == false
+// a == !a
+
+//✅SOLUTION:
+const a = " "
+
+console.log(a == false)
+console.log(!a == false)
+console.log(a == !a)
