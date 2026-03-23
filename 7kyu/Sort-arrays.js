6fe9c51175a985e05c201bf180fa567bcd873fbe
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2026-03-23 21:40:33 +0300

    new commit

---
+++ 7kyu/Sort-arrays.js
@@ -0,0 +1,13 @@
+//❓Простой пример использования сортировки. Создайте функцию, которая возвращает элементы входного массива/списка, отсортированные в лексикографическом порядке.
+
+// ✅ SOLUTION:
+// input: names - unsorted array
+// output: sorted array
+// sortme = function( names ){
+//
+// }
+
+const sortme = (names) => {
+    return names.sort();
+}
+console.log(sortme(['one', 'two', 'three' ]));
