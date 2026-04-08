fa20109ff1c4f363c08e6d86193aeed4c5682da4
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2026-04-08 21:34:06 +0300

    new commit

---
+++ 7kyu/IsUndefined.js
@@ -0,0 +1,6 @@
+//❓Функция должна проверять, является ли входной сигнал undefinedпустым, но она работает некорректно. Можете исправить код?
+
+// ✅ SOLUTION:
+function isUndefined(value) {
+    return value === undefined;
+}
